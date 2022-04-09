7d72cf99dd45f43fa03ced555d64745f44fe0d94
t-nakamura <im9today@me.com>
2022-04-09 11:31:42 +0900

    first commit

---
+++ src/pages/About.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1>About</h1>
+  <p>
+    <img src="../assets/logo.png" alt="logo" />
+  </p>
+  <HelloWorld />
+</template>
+
+<style scoped>
+h1,
+a {
+  color: green;
+}
+</style>
